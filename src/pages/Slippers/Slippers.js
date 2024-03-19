69dee36e34a27204aa522d2e2e9682223da63cf1
Aleksandr <peenky88@yandex.ru>
2024-03-19 21:31:22 +0500

    upd slippers

--- src/pages/Slippers/Slippers.js
+++ src/pages/Slippers/Slippers.js
@@ -2,13 +2,13 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import ProductCard from "../../components/Card/ProductCard";
 
-const Snippers = () => {
-  const [snippers, setSnippers] = useState([]);
+const Slippers = () => {
+  const [slippers, setSlippers] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios("https://cf24c06c8bbe52a6.mokky.dev/snippers");
-      setSnippers(result.data);
+      const result = await axios("https://cf24c06c8bbe52a6.mokky.dev/slippers");
+      setSlippers(result.data);
     };
 
     fetchData();
@@ -23,11 +23,11 @@ const Snippers = () => {
         justifyContent: "center",
       }}
     >
-      {snippers.map((snipper) => (
-        <ProductCard key={snipper.id} product={snipper} category="snipper" />
+      {slippers.map((slipper) => (
+        <ProductCard key={slipper.id} product={slipper} category="slipper" />
       ))}
     </section>
   );
 };
 
-export default Snippers;
+export default Slippers;
